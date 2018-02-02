d08348221f8edd52b2208e94d96bfc76da6bd6e5
lexkoma <amoraleks@gmail.com>
2018-02-02 22:48:52 +0200

    class name changing

--- src/airlinecompany/enums/comandsenum/AddAircraftsEnum.java
+++ src/airlinecompany/enums/comandsenum/AddAircraftsEnum.java
@@ -7,7 +7,6 @@ public enum AddAircraftsEnum {
     ADD_NEW_PASSENGER_AIRCRAFT, ADD_NEW_CARGO_AIRCRAFT;
 
     // Returns a type of enum to create a new Aircraft object according
-
     public static AddAircraftsEnum stringToEnum(String commandString) {
         switch (commandString) {
             case "1": {
